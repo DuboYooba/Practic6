3568474ac222aaef1ecaf4df531c3636404a106a
DuboYooba <pwnagehentgen00@gmail.com>
2022-12-03 20:53:15 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = {45, 59, 12, 60, 1, 47, 1428, 925 };
+            for (int i = 0; i < myArray.Length; i++)
+                Console.Write(myArray[i] + " ");
+            int max = 0;
+            for (int i = 0; i < myArray.Length; ++i)
+                if (myArray[i] > myArray[max])
+                    max = i;
+            myArray[0] ^= myArray[max];
+            myArray[max] ^= myArray[0];
+            myArray[0] ^= myArray[max];
+            Console.Write("\n-------------------------\n");
+            for (int i = 0; i < myArray.Length; i++)
+                Console.Write(myArray[i] + " ");
+            Console.Write("\n");
+        }   
+    }
+}
---
+++ Massive_PR6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива до числа: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] myArray = Enumerable.Range(1, size).ToArray();
+            for (int i = 0; i < myArray.Length; i++) 
+            {
+                Console.Write(myArray[i]+ "  " );
+            }
+            Console.Write("\nВведите число: ");
+            int number = int.Parse(Console.ReadLine());
+            while (Enumerable.Range(1, size).Contains(number))
+            {
+                number -= 1;
+                Console.WriteLine($"Число {number} x2 {number * 2}");
+            }
+            
+            
+        }
+    }
+}
